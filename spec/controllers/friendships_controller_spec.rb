fa8a0ec5c8fb9179265bf6ed715eb8a5a316023b
firmo holanda <firmo@viize.com>
2020-03-10 19:30:40 -0300

    create spect tests for models

--- spec/controllers/friendships_controller_spec.rb
+++ spec/controllers/friendships_controller_spec.rb
@@ -1,33 +1,26 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe FriendshipsController, type: :controller do
-
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
-    end
+  
+  before :each do
+    @user1 = User.create(name: "user01", email: "example-01@example.com", password: "password", password_confirmation: "password")
+    @user2 = User.create(name: "user02", email: "example-02@example.com", password: "password", password_confirmation: "password")
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
+  it "# creates a valid approved_friendship" do    
+    approved_friendship = @user1.friendships.create(friend_id: @user2.id, approved: true)
+    approved_friendship.approved = true
+    expect(approved_friendship).to be_valid
   end
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
-    end
+  it "# creates a valid pending_friendship" do
+    pending_friendship = @user1.friendships.create(friend_id: @user2.id, approved: false)
+    expect(pending_friendship).to be_valid
   end
 
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
+  it "# creates a invalid friendship" do
+    friendship = Friendship.new(friend_id: "", approved: false)
+    expect(friendship).to_not be_valid
   end
 
 end
--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -1,5 +1,18 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Friendship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'Association tests' do
+    describe 'friendship model associations' do
+      it 'belongs to sender' do
+        assc = Friendship.reflect_on_association(:user)
+        expect(assc.macro).to eq :belongs_to
+      end
+      it 'belongs to reciever' do
+        assc = Friendship.reflect_on_association(:friend)
+        expect(assc.macro).to eq :belongs_to
+      end
+    end
+  end
+
 end
