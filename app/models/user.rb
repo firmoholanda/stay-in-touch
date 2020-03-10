b3c0de929a7b28b62c9b407fe9b3f98cc0a27c37
firmo holanda <firmo@viize.com>
2020-03-10 18:03:40 -0300

    create friendship and users helpers

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, length: { maximum: 20 }
+  validates :name, presence: true, length: { maximum: 50 }
 
   has_many :posts
   has_many :comments, dependent: :destroy
@@ -12,4 +12,15 @@ class User < ApplicationRecord
 
   has_many :friendships
   has_many :inverse_friendships, class_name: "Friendship", :foreign_key => 'friend_id'
+
+  def friends
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.approved}
+    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.approved}
+    friends_array.compact
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
+
 end
