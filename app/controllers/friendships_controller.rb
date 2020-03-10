a69ad59f908ae251b449e1ebdb4f8f6703738885
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-03-10 16:57:17 -0400

    Add the index and create actions in the friendship controller

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,13 +1,19 @@
 class FriendshipsController < ApplicationController
   def new
+    @friendship = Friendship.new
   end
 
   def create
+    user = User.find(params[:id])
+    friendship = curent_user.friendships.create(friend_id: user.id, approved: false)
+    if friendship.save
+      redirect_to request.referrer, notice: "friend request was sent."
+    else
+      redirect_to :back, alert: "friend request failed."
+    end
   end
 
   def index
-  end
-
-  def destroy
+    @friendships = current_user.friendships
   end
 end
