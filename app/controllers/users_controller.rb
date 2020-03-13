a73361829caf564187289ad436d97e6a78489a1e
firmo holanda <firmo@viize.com>
2020-03-13 13:13:40 -0300

    edit seeds and pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.paginate(page: params[:page], per_page: 10)
+    @users = User.paginate(page: params[:page], per_page: 5)
     @friendships = current_user.friendships
     @inverse_friendships = current_user.inverse_friendships
   end
